ca020254e9a991a80efc56b6a136f7ef6b6e4da4
Gwennael Jean <gwennael.jean@linkt.fr>
2023-11-14 10:24:39 +0100

    feat(analytics): utilisation du client HTTP de Symfony plutôt que de la function `file_get_contents()` de PHP

--- src/Service/Analytics/AnalyticsInterface.php
+++ src/Service/Analytics/AnalyticsInterface.php
@@ -2,11 +2,18 @@
 
 namespace App\Service\Analytics;
 
+use App\Service\Analytics\Exception\DataUnavailableException;
 use App\Service\Analytics\Model\VisitCollection;
 
 interface AnalyticsInterface
 {
+    /**
+     * @throws DataUnavailableException
+     */
     public function getVisits(): VisitCollection;
 
+    /**
+     * @throws DataUnavailableException
+     */
     public function getUniqueVisits(): VisitCollection;
 }
